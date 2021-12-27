47361e9433f1242e28699f8dbcf4c8cd5f05565b
HeeSeok-Kwon <simondodream@gmail.com>
2021-12-27 09:53:54 +0900

    signin-signup-logout update

--- MentoMenti/app/src/main/java/com/example/mentomenti/MainActivity.kt
+++ MentoMenti/app/src/main/java/com/example/mentomenti/MainActivity.kt
@@ -4,10 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.service.autofill.FieldClassification
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.mentomenti.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private var auth : FirebaseAuth? = null
 
     private val MatchingPage : Fragment by lazy { com.example.mentomenti.setting.MatchingPage() }
     private val ChattingPage : Fragment by lazy { com.example.mentomenti.setting.ChattingPage() }
@@ -16,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding;
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        auth = Firebase.auth
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         var view = binding.root
@@ -25,11 +32,21 @@ class MainActivity : AppCompatActivity() {
         val preferences = getSharedPreferences("firstCheck", MODE_PRIVATE)
         var editor = preferences.edit()
         var firstViewShow : Boolean = preferences.getBoolean("first", false)
+        val logoutTextView: TextView = findViewById(R.id.logoutTextView)
 
         if (!firstViewShow) {
-            editor.putBoolean("first",true).apply()
-            var firstIntent = Intent(applicationContext,SignUpActivity::class.java)
+             editor.putBoolean("first",true).apply()
+            var firstIntent = Intent(applicationContext, SignInActivity::class.java)
             startActivity(firstIntent)
+        }
+
+        // 로그아웃
+        logoutTextView.setOnClickListener {
+            // 로그인 화면으로
+            val intent = Intent(this, SignInActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+            auth?.signOut()
         }
     }
 
