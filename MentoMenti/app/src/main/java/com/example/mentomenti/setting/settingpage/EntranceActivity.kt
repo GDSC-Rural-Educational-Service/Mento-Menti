1a5dc0484b01d9d85cbf53337c8f1c8b9433818d
HeeSeok-Kwon <simondodream@gmail.com>
2021-12-30 09:21:16 +0900

    susi, jeongsi update

--- MentoMenti/app/src/main/java/com/example/mentomenti/setting/settingpage/EntranceActivity.kt
+++ MentoMenti/app/src/main/java/com/example/mentomenti/setting/settingpage/EntranceActivity.kt
@@ -1,24 +1,37 @@
 package com.example.mentomenti.setting.settingpage
 
-import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.content.ContextCompat
+import com.example.mentomenti.MainActivity
 import com.example.mentomenti.R
+import com.example.mentomenti.setting.SettingPage
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 class EntranceActivity : AppCompatActivity() {
+    var way : String = ""
+    private lateinit var fbFirestore : FirebaseFirestore
+    private val db = FirebaseFirestore.getInstance()
+    private lateinit var currentUser: String
+    private var auth : FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entrance)
+
+        auth = Firebase.auth
+        currentUser = auth?.currentUser!!.email.toString()
+        fbFirestore = FirebaseFirestore.getInstance()
 
         supportActionBar?.title = "정보 수정하기"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -35,6 +48,7 @@ class EntranceActivity : AppCompatActivity() {
             if (idCheck == null || clicked == false){ // 아무 것도 클릭 안되어 있음
                 idCheck = susi
                 clicked = true
+                way = "susi"
                 susi.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.btn_unclicked))
                 susi.setTextColor(Color.rgb(92,196,133))
             }
@@ -56,6 +70,7 @@ class EntranceActivity : AppCompatActivity() {
             if (idCheck == null || clicked == false){ // 아무 것도 클릭 안되어 있음
                 idCheck = jeongsi
                 clicked = true
+                way = "jeongsi"
                 jeongsi.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.btn_unclicked))
                 jeongsi.setTextColor(Color.rgb(92,196,133))
             }
@@ -78,7 +93,6 @@ class EntranceActivity : AppCompatActivity() {
         else if(idCheck == jeongsi) {
             gender = 1
         }
-
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.setting_detail, menu)
@@ -86,10 +100,25 @@ class EntranceActivity : AppCompatActivity() {
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle presses on the action bar items
+        var map = mutableMapOf<String, Any>()
+        map["way"] = ""
+        if(way == "susi") {
+            map["way"] = "수시"
+        } else if(way == "jeongsi"){
+            map["way"] = "정시"
+        } else {
+            Toast.makeText(
+                baseContext, "수시, 정시를 선택해주세요!",
+                Toast.LENGTH_SHORT
+            ).show()
+            return false
+        }
         when(item.itemId){
             R.id.fixActionBtn -> {
+                db.collection("mentoinfo").document(currentUser).update(map)
                 finish()
             }}
         return super.onOptionsItemSelected(item)
     }
+
 }
